c867c8882be60e56c467012f6a550f03c82a3ec2
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-03-08 21:41:34 +0200

    Update logoutUser.js

--- controllers/users/logoutUser.js
+++ controllers/users/logoutUser.js
@@ -3,7 +3,7 @@ const { User } = require("../../models/");
 const logoutUser = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, {
-    token: "",
+    accessToken: "",
   });
   res.status(204);
 };
