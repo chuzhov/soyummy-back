93d4a0906dcafef1a0c45f26ecf4ce5a7d26a9ea
HollowMax <maxholovatyi5@gmail.com>
2023-03-09 11:41:29 +0200

    Bug fixed

--- controllers/popular/popular.js
+++ controllers/popular/popular.js
@@ -3,11 +3,8 @@ const { popularMeals } = require('../../models/popularMeals');
 
 const popular = async (req, res, next) => {
   const data = await popularMeals.find({}, '-_id -requestCount').sort({ requestCount: -1 });
-  //   const f = data.sort((a, b) => b.requestCount - a.requestCount);
-  //   console.log(f);
-  const flatData = data.flatMap(data => data.mealId);
 
-  res.json({ data: flatData });
+  res.json({ meals: data });
 };
 
 module.exports = { popular: ctrl(popular) };
