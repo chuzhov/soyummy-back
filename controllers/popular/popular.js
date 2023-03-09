739e1aa8fb77db5341254ed8a9dcba62e6cbca84
HollowMax <maxholovatyi5@gmail.com>
2023-03-09 12:47:35 +0200

    Add limit count

--- controllers/popular/popular.js
+++ controllers/popular/popular.js
@@ -4,9 +4,11 @@ const { popularMeals } = require('../../models/popularMeals');
 const popular = async (req, res, next) => {
   const data = await popularMeals.find({}, '-_id -requestCount').sort({ requestCount: -1 });
 
-  const limit = data.slice(0, 10);
+  const limitCount = 10;
 
-  res.json({ meals: limit });
+  const limitedData = data.slice(0, limitCount);
+
+  res.json({ meals: limitedData });
 };
 
 module.exports = { popular: ctrl(popular) };
