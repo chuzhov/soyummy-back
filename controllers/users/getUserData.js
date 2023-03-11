62a34d8ffb64b2716c28c581a109cec07d3ea913
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-03-11 11:17:06 +0200

    update get-user method
    Switches from POST to GET method;
    Updating params to prevent access to user's data from browser's address/search line

--- controllers/users/getUserData.js
+++ controllers/users/getUserData.js
@@ -1,7 +1,15 @@
 const { User } = require("../../models/");
+const {
+  HttpError,
+} = require("../../routes/errors/HttpErrors");
 
 const getUserData = async (req, res) => {
   const { id } = req.user;
+  const { uid } = req.params;
+
+  if (id !== uid){
+    throw HttpError(404)
+  }
   
   const dbAnswer = await User.findOne({_id: id }).select("_id name email avatarURL favoriteRecipes");
   res.json(dbAnswer);
