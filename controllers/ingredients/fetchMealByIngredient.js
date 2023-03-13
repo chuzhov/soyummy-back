3eedea0ec967e0b6ef5f311e99772b97993d2af2
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-03-13 20:22:42 +0200

    add pagination for fetchMealByIngredient

--- controllers/ingredients/fetchMealByIngredient.js
+++ controllers/ingredients/fetchMealByIngredient.js
@@ -1,17 +1,28 @@
-const instance = require('../../helpers/instance')
+const { instance , setPaginationSlice } = require('../../helpers')
 
 const {
   HttpError,
 } = require("../../routes/errors/HttpErrors");
 
 const fetchMealByIngredient = async (req, res) => {
-  const { q } = req.params;
-    const { data } = await instance.get(`/filter.php?i=${q}`)
-    if (!data.meals) {
-        throw HttpError(404, `Meal by ingredient ${q} not found`);
-    }
-    res.send(data.meals);
-};
 
+  const { q } = req.params;
+  
+  const { data } = await instance.get(`/filter.php?i=${q}`)
+  if (!data.meals) {
+    throw HttpError(404, `Meal with ingredient ${q} not found`);
+  }
+
+  const { page = 1, per_page = data.meals.length } = req.query;
+  const pagination = setPaginationSlice( page, per_page, data.meals.length );
+  if ( !pagination ) {
+    throw HttpError(400, `Incorrect pagination params`);
+  }
+  res.send({
+    "totalHits": data.meals.length,
+    "meals": data.meals.slice(pagination.start, pagination.end)
+  });
+
+};
 
 module.exports = fetchMealByIngredient;
