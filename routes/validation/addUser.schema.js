5adbe01bc7dfe4a1a78be36d66171f27fc44c732
Maksym <PTNPNH722@gmail.com>
2023-03-13 18:13:37 +0200

    fix

--- routes/validation/addUser.schema.js
+++ routes/validation/addUser.schema.js
@@ -1,7 +1,6 @@
 const Joi = require("joi");
 
 const addUserSchema = Joi.object().keys({
-  avatarURL:Joi.string(),
   name: Joi.string().min(3).required(),
   password: Joi.string().min(6).required(),
   email: Joi.string()
