0096217783b94fb926230c4261662f5e74ebf794
Maksym <PTNPNH722@gmail.com>
2023-03-14 13:28:41 +0200

    add Recipe fix

--- controllers/recipes/addRecipe.js
+++ controllers/recipes/addRecipe.js
@@ -10,44 +10,39 @@ const {
 
 
 const addRecipe = async (req, res) => {
-    
   const { _id } = req.user;
-  const recipe = req.body;
-  const { ingredients } = req.body;
+  const { ingredients, ...recipe } = req.body;
   const ingredientList = await instance.get('/list.php?i=list');
   const pictureURL = req.file?.path || DEFAULT_RECIPE_IMG_URL;
   
-  const foundIngredients = [];
+  const foundIngredients = ingredients
+    .map(({ ingredient, qty }) => {
+      const foundIngredient = ingredientList.data.meals.find(item => item.strIngredient === ingredient);
+      return foundIngredient && {
+        ingredient,
+        qty,
+        description: foundIngredient.strDescription,
+        imgURL: `${BASE_INGREDIENT_IMG_URL}/${ingredient.replace(/\s/g, '%20')}-Small.png`,
+        id: foundIngredient.idIngredient
+      };
+    })
+    .filter(Boolean);
 
-  for (let i = 0; i < ingredients.length; i++) {
-    const ingredient = ingredients[i].ingredient;
-    const foundIngredient = ingredientList.data.meals.find(item => item.strIngredient === ingredient);
+   const conditions = foundIngredients.map(obj => ({
+     $elemMatch: obj
+   }))
 
-    if (foundIngredient) {
-      foundIngredients.push(
-        {
-          ingredient: ingredients[i].ingredient,
-          qty: ingredients[i].qty,
-          description: foundIngredient.strDescription,
-          imgURL: `${BASE_INGREDIENT_IMG_URL}/${ingredient.replace(/\s/g, '%20')}-Small.png`,
-          id: foundIngredient.idIngredient
-        }
-      );
-    }
-  };
+  const result = await Recipe.findOne({owner: _id ,ingredients: { $all: conditions } });
 
-  const conditions = foundIngredients.map(obj => ({
-    $elemMatch: obj
-  }))
-
-  const result = await Recipe.find({ ingredients: { $all: conditions } });
-
-  if (result.length > 0) {
-    throw HttpError(409, "Recipe is already created")
+  if (result) {
+    throw HttpError(409, "Recipe is already created");
   }
-  await Recipe.create({ ...recipe, owner: _id, imgURL: pictureURL, ingredients: foundIngredients} );
-    res.status(201).send('Recipe created successfully');
+
+  await Recipe.create({ ...recipe, owner: _id, imgURL: pictureURL, ingredients: foundIngredients });
+  res.status(201).send('Recipe created successfully');
 };
 
 module.exports = addRecipe;
+
+
 
