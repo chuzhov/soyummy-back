39d51e03ef71e042b1db10b1759b6d658f6179cd
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-03-16 19:55:33 +0200

    Refactor fetch function

--- controllers/ingredients/fetchAll.js
+++ controllers/ingredients/fetchAll.js
@@ -1,16 +1,13 @@
-const instance = require('../../helpers/instance')
+const { fetchIngredientsList } = require('../../helpers');
 
-const {
-  HttpError,
-} = require("../../routes/errors/HttpErrors");
+const { HttpError } = require('../../routes/errors/HttpErrors');
 
 const fetchAll = async (req, res) => {
-  const { data } = await instance.get(`/list.php?i=list`) 
+  const { data } = await fetchIngredientsList();
   if (!data) {
     throw HttpError(500, 'Ingredients not found');
   }
   res.send(data);
 };
-
 
 module.exports = fetchAll;
