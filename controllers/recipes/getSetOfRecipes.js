b49d7ad602dd4a1ff540fb7718b9ad7136a29b1a
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-03-22 11:20:15 +0200

    Bugfix

--- controllers/recipes/getSetOfRecipes.js
+++ controllers/recipes/getSetOfRecipes.js
@@ -8,18 +8,31 @@ const getSetOfRecipes = async (req, res, next) => {
   const mealsByCategory = {};
   let responseArray = [];
 
+  // for (const category of MAIN_CATEGORIES) {
+  //   const { meals } = await fetchRecipesByCategory(category);
+  //   if (meals) {
+  //     const randomMealsIndexes = randomizeArray(
+  //       meals.length,
+  //       Math.min(MEALS_PER_CATEGORY, meals.length)
+  //     );
+  //     for (let index = 0; index < MEALS_PER_CATEGORY; index++) {
+  //       responseArray.push(meals[randomMealsIndexes[index]]);
+  //     }
+  //   } else {
+  //     responseArray[category] = [];
+  //   }
+  //   mealsByCategory[category] = responseArray;
+  //   responseArray = [];
+
   for (const category of MAIN_CATEGORIES) {
     const { meals } = await fetchRecipesByCategory(category);
     if (meals) {
-      const randomMealsIndexes = randomizeArray(
-        meals.length,
-        Math.min(MEALS_PER_CATEGORY, meals.length)
+      responseArray = meals.slice(
+        0,
+        Math.min(meals.length, MEALS_PER_CATEGORY)
       );
-      for (let index = 0; index < MEALS_PER_CATEGORY; index++) {
-        responseArray.push(meals[randomMealsIndexes[index]]);
-      }
     } else {
-      responseArray[category] = [];
+      responseArray = [];
     }
     mealsByCategory[category] = responseArray;
     responseArray = [];
