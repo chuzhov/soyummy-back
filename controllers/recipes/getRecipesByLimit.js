6900f1bec1aa71e2efadb8f2d6bab102ae40fdb1
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-03-22 11:38:22 +0200

    cancel random

--- controllers/recipes/getRecipesByLimit.js
+++ controllers/recipes/getRecipesByLimit.js
@@ -8,24 +8,24 @@ const getRecipesByLimit = async (req, res, next) => {
   const data = await fetchRecipesByCategory(category);
 
   const limit = Number(req.params.limit);
-  let meals;
+  //let meals;
 
-  if (limit === 4 || limit === 12) {
-    const randArr = randomizeArray(data.meals.length, limit);
-    if (randArr.length === limit) {
-      meals = [];
+  // if (limit === 4 || limit === 12) {
+  //   const randArr = randomizeArray(data.meals.length, limit);
+  //   if (randArr.length === limit) {
+  //     meals = [];
 
-      for (const num of randArr) {
-        meals.push(data.meals[num]);
-      }
-    } else {
-      meals = data.meals.slice(0, limit);
-    }
+  //     for (const num of randArr) {
+  //       meals.push(data.meals[num]);
+  //     }
+  //   } else {
+  //     meals = data.meals.slice(0, limit);
+  //   }
 
-    res.json({ meals });
-  } else {
-    throw HttpError(400);
-  }
+  res.json({ meals: meals.slice(0, Math.min(meals.length, limit)) });
+  // } else {
+  //   throw HttpError(400);
+  // }
 };
 
 module.exports = getRecipesByLimit;
